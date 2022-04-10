95edb3ab08b1f35847047630a49d2f9afb173ebf
Ankie Pon <anpon@ucsc.edu>
2022-04-10 15:37:47 -0700

    got up to parsing keyboard input

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 
 //game declaration
 let game = new Phaser.Game(config);
+
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//set UI sizes
+let borderUISize= game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
