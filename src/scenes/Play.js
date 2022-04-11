159ce9fd964e128299f16c8ecd74712783999ee9
Ankie Pon <anpon@ucsc.edu>
2022-04-11 16:00:14 -0700

    test push

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -66,7 +66,7 @@ class Play extends Phaser.Scene {
 
         //60 second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(3000, () => {
+        this.clock = this.time.delayedCall(60000, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
@@ -135,5 +135,7 @@ class Play extends Phaser.Scene {
         //scpre add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+
+        this.sound.play('sfx_explosion');
     }
 }
