8890305ca4b4efae29026ea1c897e8f46a00518f
Ankie Pon <anpon@ucsc.edu>
2022-04-18 03:42:46 -0700

    implemented, need to fix

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,8 +4,8 @@ class Play extends Phaser.Scene {
     }
     preload() {
         //load images/tile sprites
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('newRocket', './assets/rocket.png');
+        this.load.image('newShip', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
